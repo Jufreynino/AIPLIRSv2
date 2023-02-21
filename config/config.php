29bc2b2203f9dac7b7184bb0188bfab8df8b30a3
Jufrey Niño E. Bayog <92130085+Jufreynino@users.noreply.github.com>
2023-02-21 09:35:07 +0800

    try catch applied on db

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@ $server = 'localhost';
 $root = 'root';
 $pass = 'nmis@admin123!';
 $db = 'aiplirs_db';
-$con = mysqli_connect($server,$root,$pass,$db);
+$con = mysqli_connect($server,$root,$pass,$db)or die("Unable to connect with Database"); ;
 ?>
